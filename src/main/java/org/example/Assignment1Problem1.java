69ff6123f3e4091d66eaee14aea6e2c2ef02987b
Omkar Sunil Lolage <lolageomkar5@gmail.com>
2025-01-13 10:57:30 +0530

    Adds two problems of first lab assignment

---
+++ src/main/java/org/example/Assignment1Problem1.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Assignment1Problem1 {
+    public static double calculateAverageUpdates(int n) {
+        int totalUpdates = n;
+        for (int i = 1; i <= n - 1; i++) {
+          totalUpdates += i * (i + 1);
+        }
+        long factorial = 1;
+        for (int i = 2; i <= n; i++) {
+          factorial *= i;
+        }
+        System.out.println(totalUpdates + "/" + factorial);
+        return (double) totalUpdates / factorial;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double averageUpdates = calculateAverageUpdates(n);
+        System.out.println("Average number of updates to the max variable per permutation for n = " + n + " is: " + averageUpdates);
+    }
+}
---
+++ src/main/java/org/example/Assignment1Problem2.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Assignment1Problem2 {
+
+    public static long countDerangements(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 0;
+
+        long Dn_2 = 1;
+        long Dn_1 = 0;
+        long Dn = 0;
+
+        // Use the recurrence relation D_n = (n-1) * (D_{n-1} + D_{n-2})
+        for (int i = 2; i <= n; i++) {
+            Dn = (i - 1) * (Dn_1 + Dn_2);
+            Dn_2 = Dn_1;
+            Dn_1 = Dn;
+        }
+
+        return Dn;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+
+        long goodPermutations = countDerangements(n);
+        System.out.printf("The number of good permutations (derangements) for n = %d is: %d\n", n, goodPermutations);
+
+        scanner.close();
+    }
+}
